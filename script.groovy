04972cd4f4742370d8a7566cfc665170cb62aefe
sukethanreddy <95679854+sukethanreddy@users.noreply.github.com>
2021-12-16 00:27:07 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,5 +1,8 @@
 def build() {
-    sh 'mvn clean package '
+    sh 'mvn clean package'
+}
+def sonar() {
+    sh sh 'mvn sonar:sonar -Dsonar.host.url=http://3.91.232.116:9000 -Dsonar.login=13452f0700fb5aed47696638ee052d926d73b36e'
 }
 
 return this
